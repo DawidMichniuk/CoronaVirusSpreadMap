ca03ac5d993afe9104b406b87caf7fd14eacdae2
fategas <normalplek@gmail.com>
2020-02-08 01:47:14 +0000

    Info on the data

--- coronavirus.py
+++ coronavirus.py
@@ -1,6 +1,7 @@
 import plotly.graph_objects as go
 import pandas as pd
 
+# here's the link to a spreadsheet with the data. Latest entry is 6/02/2020 as of 8/02/2020
 url='https://docs.google.com/spreadsheets/d/18X1VM1671d99V_yd-cnUI1j8oSG2ZgfU_q1HfOizErA/export?format=csv&id'
 data=pd.read_csv(url)
 data=data.fillna(0)
